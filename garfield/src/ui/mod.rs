c7b3b57aa1caea630270150b8bedc0775ba050ab
mfwolffe <wolffemf@dukes.jmu.edu>
2026-01-22 15:25:28 -0500

    ui: export new view components (grid, column, status bar)

--- garfield/src/ui/mod.rs
+++ garfield/src/ui/mod.rs
@@ -2,10 +2,16 @@
 
 pub mod address_bar;
 pub mod breadcrumb;
+pub mod column_view;
+pub mod grid_view;
 pub mod list_view;
 pub mod sidebar;
+pub mod status_bar;
 
 pub use address_bar::AddressBar;
 pub use breadcrumb::Breadcrumb;
+pub use column_view::ColumnView;
+pub use grid_view::GridView;
 pub use list_view::ListView;
 pub use sidebar::Sidebar;
+pub use status_bar::StatusBar;
