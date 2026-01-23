96a17c4cbeff320221fc681023a69949fd7e5019
mfwolffe <wolffemf@dukes.jmu.edu>
2026-01-23 13:47:14 -0500

    ui: export ColumnClickResult and HelpModal

--- garfield/src/ui/mod.rs
+++ garfield/src/ui/mod.rs
@@ -4,6 +4,7 @@ pub mod address_bar;
 pub mod breadcrumb;
 pub mod column_view;
 pub mod grid_view;
+pub mod help_modal;
 pub mod list_view;
 pub mod pane;
 pub mod sidebar;
@@ -14,8 +15,9 @@ pub mod toolbar;
 
 pub use address_bar::AddressBar;
 pub use breadcrumb::Breadcrumb;
-pub use column_view::ColumnView;
+pub use column_view::{ColumnClickResult, ColumnView};
 pub use grid_view::GridView;
+pub use help_modal::HelpModal;
 pub use list_view::ListView;
 pub use pane::Pane;
 pub use sidebar::Sidebar;
